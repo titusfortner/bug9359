02b43eca4b95d1eff60236b4caed06557ea40338
titusfortner <titusfortner@gmail.com>
2021-04-22 10:06:53 -0500

    add tests

---
+++ src/test/java/com/titusfortner/bug9359/ClassFiveTest.java
@@ -0,0 +1,46 @@
+package com.titusfortner.bug9359;
+
+import org.junit.jupiter.api.Test;
+
+public class ClassFiveTest extends BaseTest {
+
+    @Test
+    public void methodOneTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodTwoTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodThreeTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodFourTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodFiveTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodSixTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodSevenTest() {
+        driver.get("https://nytimes.com");
+    }
+
+    @Test
+    public void methodEightTest() {
+        driver.get("https://nytimes.com");
+    }
+}
